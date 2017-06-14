15e582773bfbc457ba6e7686348dc31dd1070430
Danylo Medinski <medinsky94@gmail.com>
2017-06-14 01:17:55 -0700

    Add files via upload

--- Site/SQL/insert.sql
+++ Site/SQL/insert.sql
@@ -1,39 +1,85 @@
 INSERT INTO ARTICLE (
   TITLE,
-  CONTENT
+  CONTENT,
+  ARTICLE_IMAGE
 )VALUES(
   'Article 1',
   '<p>Content Content Content Content Content Content Content 
    Content Content Content Content Content Content Content Content
-   Content</p> <p>Content Content Content Content Content Content</p>'
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content</p> <p>Content Content Content Content Content Content</p>',
+   '/Site/Media/Article/1/6932276295_e76a8c3455_o.jpg'
 );
 
 INSERT INTO ARTICLE (
   TITLE,
-  CONTENT
+  CONTENT,
+  ARTICLE_IMAGE
 )VALUES(
   'Article 2',
   '<p>Content Content Content Content Content Content Content 
    Content Content Content Content Content Content Content Content
-   Content</p> <p>Content Content Content Content Content Content</p>'
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content</p> <p>Content Content Content Content Content Content</p>',
+   '/Site/Media/Article/2/34183849751_51412b63ef_o.jpg'
 );
 
 INSERT INTO ARTICLE (
   TITLE,
-  CONTENT
+  CONTENT,
+  ARTICLE_IMAGE
 )VALUES(
   'Article 3',
   '<p>Content Content Content Content Content Content Content 
    Content Content Content Content Content Content Content Content
-   Content</p> <p>Content Content Content Content Content Content</p>'
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content</p> <p>Content Content Content Content Content Content</p>',
+   '/Site/Media/Article/3/4847537363_3b20e01741_o.jpg'
+);
+
+INSERT INTO ARTICLE (
+  TITLE,
+  CONTENT,
+  ARTICLE_IMAGE
+)VALUES(
+  'Article 4',
+  '<p>Content Content Content Content Content Content Content 
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content</p> <p>Content Content Content Content Content Content</p>',
+   '/Site/Media/Article/4/bg.jpg'
 );
 
 INSERT INTO ARTICLE (
   TITLE,
   CONTENT
 )VALUES(
-  'Article 4',
+  'Article 5',
   '<p>Content Content Content Content Content Content Content 
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
+   Content Content Content Content Content Content Content Content
    Content Content Content Content Content Content Content Content
    Content</p> <p>Content Content Content Content Content Content</p>'
 );
